ef06917bfd1b0a5239cc982db3c7f126bbe80530
Mauricio Linhares <mlinhares@neat.com>
2014-08-11 18:58:21 -0300

    Sync STDOUT so the logger is always up to date

--- lib/cloud_formation/bridge/cli.rb
+++ lib/cloud_formation/bridge/cli.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'cloud_formation/bridge/poller'
+require 'cloud_formation/bridge/util'
 
 module CloudFormation
   module Bridge
@@ -7,7 +8,8 @@ module CloudFormation
 
       desc "start QUEUE_NAME", "Starts watching this specific SQS queue"
       def start(queue_name)
-        poller = CloudFormation::Bridge::Poller.new(queue_name)
+        STDOUT.sync = true
+        poller = CloudFormation::Bridge::Poller.new(queue_name, Util::LOGGER)
         poller.start
       end
 
--- lib/cloud_formation/bridge/version.rb
+++ lib/cloud_formation/bridge/version.rb
@@ -1,5 +1,5 @@
 module CloudFormation
   module Bridge
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end
