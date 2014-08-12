b74de19a6f3ddf8813efae9a67659ca287b31472
Mauricio Linhares <mlinhares@neat.com>
2014-08-11 22:38:28 -0300

    Print version when you start

--- lib/cloud_formation/bridge/cli.rb
+++ lib/cloud_formation/bridge/cli.rb
@@ -1,6 +1,7 @@
 require 'thor'
 require 'cloud_formation/bridge/poller'
 require 'cloud_formation/bridge/util'
+require 'cloud_formation/bridge/version'
 
 module CloudFormation
   module Bridge
@@ -9,6 +10,9 @@ module CloudFormation
       desc "start QUEUE_NAME", "Starts watching this specific SQS queue"
       def start(queue_name)
         STDOUT.sync = true
+
+        Util::LOGGER.info("Starting cfn-bridge with queue #{queue_name} - version #{CloudFormation::Bridge::VERSION}")
+
         poller = CloudFormation::Bridge::Poller.new(queue_name)
         poller.start
       end
