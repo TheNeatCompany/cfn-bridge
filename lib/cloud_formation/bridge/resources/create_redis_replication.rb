9db215a6e5e4266a867207e9b337d433f1f049a3
Sergey Aksyonov <sergey@artistsoft.com>
2014-08-26 09:51:57 +0300

    Added method for update operation

--- lib/cloud_formation/bridge/resources/create_redis_replication.rb
+++ lib/cloud_formation/bridge/resources/create_redis_replication.rb
@@ -22,33 +22,77 @@ module CloudFormation
           require_fields(request, REQUIRED_FIELDS)
 
           cluster_id = request.resource_properties[CLUSTER_ID]
+          replication_id = "#{cluster_id[0,14]}replic"
 
-          master_data = client.describe_cache_clusters(
+          primary_data = client.describe_cache_clusters(
             cache_cluster_id: cluster_id,
             show_cache_node_info: true
           ).data[:cache_clusters][0]
 
-          replication_data = client.create_replication_group(
-            replication_group_id: "#{cluster_id[0,15]}r",
+          client.create_replication_group(
+            replication_group_id: replication_id,
             primary_cluster_id: cluster_id,
             replication_group_description: request.resource_properties[DESCRIPTION]
-          ).data
+          )
 
-          request.resource_properties[REPLICAS_COUNT].times do |i|
+          replicas_count = request.resource_properties[REPLICAS_COUNT]
+          1.upto(replicas_count) do |i|
             client.create_cache_cluster(
-              cache_cluster_id: "#{cluster_id[0,15]}#{i}",
-              replication_group_id: replication_data[:replication_group_id],
-              cache_node_type: master_data[:master_data],
+              cache_cluster_id: "#{cluster_id[0,14]}#{i}",
+              replication_group_id: replication_id,
+              cache_node_type: primary_data[:cache_node_type],
               engine: 'redis',
-              cache_security_group_names: master_data[:cache_security_groups].map { |e| e[:cache_security_group_name] },
-              preferred_availability_zone: master_data[:preferred_availability_zone]
+              cache_security_group_names: primary_data[:cache_security_groups].map { |e| e[:cache_security_group_name] },
+              preferred_availability_zone: primary_data[:preferred_availability_zone]
             )
           end
 
           {
             FIELDS::DATA => {
-              'Host' => master_data[:cache_nodes][0][:endpoint][:address],
-              'Port' => master_data[:cache_nodes][0][:endpoint][:port],
+              'Host' => primary_data[:cache_nodes][0][:endpoint][:address],
+              'Port' => primary_data[:cache_nodes][0][:endpoint][:port],
+            },
+            FIELDS::PHYSICAL_RESOURCE_ID => cluster_id,
+          }
+        end
+
+        def update(request)
+          require_fields(request, REQUIRED_FIELDS)
+
+          cluster_id = request.resource_properties[CLUSTER_ID]
+
+          primary_data = client.describe_cache_clusters(
+            cache_cluster_id: cluster_id,
+            show_cache_node_info: true
+          ).data[:cache_clusters][0]
+
+          replication_data = client.describe_replication_groups(
+            replication_group_id: primary_data[:replication_group_id]
+          ).data[:replication_groups][0]
+
+          real_count = replication_data[:member_clusters].count
+          new_count = request.resource_properties[REPLICAS_COUNT]
+          if real_count < new_count
+            (real_count + 1).upto(new_count) do |i|
+              client.create_cache_cluster(
+                cache_cluster_id: "#{cluster_id[0,14]}#{i}",
+                replication_group_id: replication_data[:replication_group_id],
+                cache_node_type: primary_data[:cache_node_type],
+                engine: 'redis',
+                cache_security_group_names: primary_data[:cache_security_groups].map { |e| e[:cache_security_group_name] },
+                preferred_availability_zone: primary_data[:preferred_availability_zone]
+              )
+            end
+          elsif real_count > new_count
+            (new_count + 1).upto(real_count) do |i|
+              client.delete_cache_cluster(cache_cluster_id: "#{cluster_id[0,14]}#{i}")
+            end
+          end
+
+          {
+            FIELDS::DATA => {
+              'Host' => primary_data[:cache_nodes][0][:endpoint][:address],
+              'Port' => primary_data[:cache_nodes][0][:endpoint][:port],
             },
             FIELDS::PHYSICAL_RESOURCE_ID => cluster_id,
           }
@@ -57,13 +101,13 @@ module CloudFormation
         def delete(request)
           require_fields(request, [CLUSTER_ID])
 
-          master_data = client.describe_cache_clusters(
+          primary_data = client.describe_cache_clusters(
             cache_cluster_id: request.resource_properties[CLUSTER_ID],
             show_cache_node_info: true
           ).data[:cache_clusters][0]
 
-          if master_data[:replication_group_id].present?
-            client.delete_replication_group(replication_group_id: master_data[:replication_group_id])
+          if primary_data[:replication_group_id].present?
+            client.delete_replication_group(replication_group_id: primary_data[:replication_group_id])
           end
         end
 
